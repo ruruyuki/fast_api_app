312641b36e4429009e4b567714290026381a3c13
yuki <github>
2022-02-20 10:44:33 +0900

    todoアプリ作成

--- test/test_main.py
+++ test/test_main.py
@@ -1,77 +1,76 @@
-from fastapi.testclient import TestClient
-from starlette.requests import Request
+import pytest
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from api.db import get_db, Base
 from api.main import app
-import pathlib
 
-client = TestClient(app)
-
-
-# ユーザー情報取得API 正常系テスト
-def test_site_index():
-    response = client.get("/")
-    # レスポンス検証
-    # ステータスコード
-    assert response.status_code == 200
-    # レスポンスボディ
+ASYNC_DB_URL = "sqlite+aiosqlite:///:memory:"
 
 
-# ユーザー情報取得API 異常系テスト
-# トークン違い
-def test_get_user_ABR001():
-    response = client.get("/users/abcde12345",
-                          headers={"token": "dummy_token"})
-    assert response.status_code == 400
-    assert response.json() == {"detail": "token_verification_failed"}
-
-# ユーザー情報取得API 異常系テスト
-# 存在しないユーザー指定
-def test_get_user_ABR002():
-    response = client.get(
-        "/users/hogefuga", headers={"token": "correct_token"})
-    assert response.status_code == 404
-    assert response.json() == {"detail": "user_not_found"}
-
-# ユーザー情報登録API 正常系テスト
-def test_create_user_NR001():
-    # 正常系リクエスト
-    # ヘッダーとボディは辞書型で渡す。
-    response = client.post(
-        "/users/",
-        headers={"Token": "correct_token"},
-        json={"id": "klmno12345", "name": "Kobayashi",
-              "email_address": "kobayashi@example.com"},
+@pytest.fixture
+async def async_client() -> AsyncClient:
+    # Async用のengineとsessionを作成
+    async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+    async_session = sessionmaker(
+        autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
     )
-    assert response.status_code == 200
-    assert response.json() == {
-        "id": "klmno12345",
-        "name": "Kobayashi",
-        "email_address": "kobayashi@example.com",
-    }
 
-# ユーザー情報登録API 異常系テスト
-# トークン違い
-def test_create_user_ABR001():
-    response = client.post(
-        "/users/",
-        headers={"Token": "dummy_token"},
-        json={"id": "klmno12345", "name": "Kobayashi",
-              "email_address": "kobayashi@example.com"},
-    )
-    assert response.status_code == 400
-    assert response.json() == {"detail": "token_verification_failed"}
+    # テスト用にオンメモリのSQLiteテーブルを初期化（関数ごとにリセット）
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    # DIを使ってFastAPIのDBの向き先をテスト用DBに変更
+    async def get_test_db():
+        async with async_session() as session:
+            yield session
+
+    app.dependency_overrides[get_db] = get_test_db
+
+    # テスト用に非同期HTTPクライアントを返却
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
 
 
-# ユーザー情報登録API 異常系テスト
-# 登録済みユーザーID指定
-def test_create_user_ABR002():
-    response = client.post(
-        "/users/",
-        headers={"Token": "correct_token"},
-        json={
-            "id": "abcde12345",
-            "name": "Kobayashi",
-            "email_address": "kobayashi@example.com"
-        },
-    )
-    assert response.status_code == 400
-    assert response.json() == {"detail": "user_id_duplicated"}
+import starlette.status
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+    response = await async_client.post("/tasks", json={"title": "テストタスク"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert response_obj["title"] == "テストタスク"
+
+    response = await async_client.get("/tasks")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert len(response_obj) == 1
+    assert response_obj[0]["title"] == "テストタスク"
+    assert response_obj[0]["done"] is False
+
+
+@pytest.mark.asyncio
+async def test_done_flag(async_client):
+    response = await async_client.post("/tasks", json={"title": "テストタスク2"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert response_obj["title"] == "テストタスク2"
+
+    # 完了フラグを立てる
+    response = await async_client.put("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # 既に完了フラグが立っているので400を返却
+    response = await async_client.put("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_400_BAD_REQUEST
+
+    # 完了フラグを外す
+    response = await async_client.delete("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # 既に完了フラグが外れているので404を返却
+    response = await async_client.delete("/tasks/1/done")
+    assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
+
